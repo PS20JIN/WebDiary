4a66583424dcacc0ac4c091bcdbc6de7c2948fd1
PS20JIN <nice20JIN@gmail.com>
2023-07-27 19:53:55 +0900

    without loginButton

---
+++ WebDJS/LogIn.js
@@ -0,0 +1,17 @@
+console.log('LogIN.js');
+const loginPage = document.querySelector("#loginPage")
+const id = document.querySelector("#id");
+const password = document.querySelector("#password");
+const loginButton = document.querySelector("#loginButton");
+
+if (localStorage.getItem("userId") === undefined || localStorage.getItem("userId") === null) {
+    function Login(event) {                                //조건문에서 논리연산자 사용방법.....shit fucking
+        event.preventDefault();
+        let userId = id.value;
+        let userPassword = password.value;
+        localStorage.setItem("userId", userId);
+        localStorage.setItem("userPassword", userPassword);
+    }
+    loginButton.addEventListener("click", Login);
+}
+//자동 로그인은 나중에 만들어야징
