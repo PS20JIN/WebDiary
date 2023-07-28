581a30e6e41cac2f5167f293327c52d8e6dde18d
PS20JIN <nice20JIN@gmail.com>
2023-07-28 11:35:09 +0900

    loginPage without MultiUser

--- WebDJS/LogIn.js
+++ WebDJS/LogIn.js
@@ -5,15 +5,21 @@ const password = document.querySelector("#password");
 const rememberCheck = document.querySelector("#remember-check");
 const loginButton = document.querySelector("#loginButton");
 
-if (localStorage.getItem("userId") === undefined || localStorage.getItem("userId") === null) {
-    function Login(event) {                                //조건문에서 논리연산자 사용방법.....shit fucking
-        event.preventDefault();
-        let userId = id.value;
-        let userPassword = password.value;
+function Login(event) {                                //조건문에서 논리연산자 사용방법.....shit fucking
+    event.preventDefault();
+    let userId = id.value;
+    if (localStorage.getItem("userId") != userId || localStorage.getItem("userId") === null || localStorage.getItem("userId") === undefined) {
         localStorage.setItem("userId", userId);
-        localStorage.setItem("userPassword", userPassword);
+        sessionStorage.setItem("currentUser", userId)
+    } else {
+        sessionStorage.setItem("currentUser", userId);
     }
-    loginButton.addEventListener("submit", Login);
+    window.location.href = "main.html"
 }
+loginPage.addEventListener("submit", Login);
+
+
 
 //자동 로그인은 나중에 만들어야징
+//같은 사용자끼리 id랑 password 묶는것도 만들기.(password도 이때 같이 만들기)
+//여러 사용자가 로그인하는것을 엄두에 두고 localStorage에 다양한 id를 만들 수 있게 하기.
